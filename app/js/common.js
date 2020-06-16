b78037e1b9a077394e4cb764dd557d6c380a7dad
master <rudolifrudolif@gmail.com>
2020-06-16 11:30:31 +0300

    preloader + animation + prevki

--- app/js/common.js
+++ app/js/common.js
@@ -105,5 +105,26 @@ $(function() {
   
   customInput();
   
+
+//-------------------------------анімація---------------------------------------
+  if($(".animation").hasClass('animation')){
+    var show = true;
+    var countbox = ".wrap--animation";
+    $(window).on("scroll load resize", function () {
+        if (!show) return false; // Отменяем показ анимации, если она уже была выполнена
+        var w_top = $(window).scrollTop(); // Количество пикселей на которое была прокручена страница
+        var e_top = $(countbox).offset().top; // Расстояние от блока со счетчиками до верха всего документа
+        var w_height = $(window).height(); // Высота окна браузера
+        var d_height = $(document).height(); // Высота всего документа
+        var e_height = $(countbox).outerHeight(); // Полная высота блока со счетчиками
+        if (w_top + 500 >= e_top || w_height + w_top == d_height || e_height + e_top < w_height) {
+            $('.animation').addClass('animation--active')
+        }
+    });
+  }
+
 });
 
+$(window).on('load', function(){
+  $('.preloader').delay(2000).fadeOut('slow');
+});
